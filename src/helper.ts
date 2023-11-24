aa3dc19b53e1052b9b7a6ea5a951129d9e85ebc4
Konrad Wienecke <konrad.wienecke@mac.com>
2023-11-24 20:22:08 +0100

    Remove unused package `fp-ts`

--- src/helper.ts
+++
@@ -1,7 +0,0 @@
-import { Option, none, some } from "fp-ts/lib/Option.js";
-
-export function toInt(str: string): Option<number> {
-    const num = parseInt(str);
-    if (isNaN(num)) return none;
-    else return some(num);
-}
